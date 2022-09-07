a63aa421b560e2ad4a8ca05e77aa39c32f918526
nam01636906337 <maivannam07032000h@gmail.com>
2022-09-07 20:06:30 +0700

    first commit

---
+++ php_basic.php
@@ -0,0 +1,18 @@
+<?php
+$a = 5;
+$b = 6;
+$c = $a <=> $b;
+echo $c;
+// php:
+// spaceship operator  <=>: return 3 gia tri 1, 0 -1 tuong ung voi 3 truong hop:
+// + neu trai toan tu lon ho ben phai return 1
+// + neu trai toan tu lon ho ben phai return -1
+// + bang tra ve 0
+
+
+// $x = 1;
+
+// while($x <= 10 && $x %2 == 0) {
+//   echo "The number is: $x <br>";
+//   $x++;
+// }
